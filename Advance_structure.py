480fab12e222e19500d05c8ff207994bc33483c1
Dulith Athukorala <dulithathukorala678@gmail.com>
2026-01-25 12:22:33 +0530

    Add forward method and comments to SelfAttention class

--- Advance_structure.py
+++ Advance_structure.py
@@ -11,6 +11,10 @@ class SelfAttention(nn.Module):
 
         assert (self.head_dim * heads == embed_size), "Embedding size needs to be divisible by heads"
 
-        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False) # nn.Linear -> initialize random weight matrix
         self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
         self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
+        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)  # final output linear layer
+
+    def forward(self, values, keys, queries, mask):
+
