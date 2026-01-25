48e11ef070e884c416b3886825415eacff5039a8
Dulith Athukorala <dulithathukorala678@gmail.com>
2026-01-25 09:48:19 +0530

    Implement training step and prediction logic in DecoderOnlyTransformer

--- Basic_structure.py
+++ Basic_structure.py
@@ -5,6 +5,43 @@ import torch.nn.functional as F
 from torch.optim import Adam
 from torch.utils.data import DataLoader, TensorDataset
 
+import lightning as L
+
+token_to_id = {
+    "what": 0,
+    "is": 1,
+    "statquest": 2,
+    "<EOS>": 3,
+    "awesome": 4
+}
+id_to_token = dict(map(reversed, token_to_id.items()))
+
+inputs = torch.tensor([[token_to_id["what"], ## input #1: what is statquest <EOS> awesome
+                        token_to_id["is"], 
+                        token_to_id["statquest"], 
+                        token_to_id["<EOS>"],
+                        token_to_id["awesome"]], 
+                       
+                       [token_to_id["statquest"], # input #2: statquest is what <EOS> awesome
+                        token_to_id["is"], 
+                        token_to_id["what"], 
+                        token_to_id["<EOS>"], 
+                        token_to_id["awesome"]]])
+
+labels = torch.tensor([[token_to_id["is"], 
+                        token_to_id["statquest"], 
+                        token_to_id["<EOS>"], 
+                        token_to_id["awesome"], 
+                        token_to_id["<EOS>"]],  
+                       
+                       [token_to_id["is"], 
+                        token_to_id["what"], 
+                        token_to_id["<EOS>"], 
+                        token_to_id["awesome"], 
+                        token_to_id["<EOS>"]]])
+
+dataset = TensorDataset(inputs, labels)
+dataloader = DataLoader(dataset)
 
 class PositionEncoding(nn.Module):
     def __init__(self, d_model=2, max_len=6):  # Embedding size = 2 , token length = 6
@@ -23,8 +60,8 @@ class PositionEncoding(nn.Module):
         self.register_buffer('pe', pe)  # register buffer so that it is not considered as a model parameter 
 
 
-    def forward(self, word_embedding):
-        return word_embedding + self.pe[:word_embedding.size(0), :] # Rows from 0 up to L-1 , all columns  # word_embedding.size(0) = max_len
+    def forward(self, word_embeddings):
+        return word_embeddings + self.pe[:word_embeddings.size(0), :] # Rows from 0 up to L-1 , all columns  # word_embedding.size(0) = max_len
         
 
 class Attention(nn.Module):
@@ -65,7 +102,7 @@ class DecoderOnlyTransformer(L.LightningModule):
         self.pe = PositionEncoding(max_len=max_len,d_model=d_model)  # position info for each token/word (max_len, d_model)
         self.self_attention = Attention(d_model=d_model)  # 
 
-        self.fc = nn.Linear(in_features=d_model, out_features=num_tokens)  # final linear layer to project to vocab size
+        self.fc_layer = nn.Linear(in_features=d_model, out_features=num_tokens)  # final linear layer to project to vocab size
 
         self.loss = nn.CrossEntropyLoss()  # cross entropy loss for classification
 
@@ -75,6 +112,58 @@ class DecoderOnlyTransformer(L.LightningModule):
 
         mask = torch.tril(torch.ones((token_ids.size(dim=0), token_ids.size(dim=0))))
         mask = mask == 0  # convert to boolean mask
+
+        self_attention_values = self.self_attention(positional_encoded, positional_encoded, positional_encoded, mask=mask)
+
+        residual_connection_values = positional_encoded + self_attention_values
+
+        fc_layer_output = self.fc_layer(residual_connection_values)
+
+        return fc_layer_output
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=0.1)
+
+    def training_step(self, batch, batch_idx):
+        input_tokens, labels = batch
+        outputs = self.forward(input_tokens)
+        loss = self.loss(outputs,labels[0])
+
+        return loss
+
+
+model = DecoderOnlyTransformer(num_tokens=len(token_to_id), d_model=2, max_len=6)
+trainer = L.Trainer(max_epochs=30)
+trainer.fit(model, train_dataloaders=dataloader)
+
+model_input = torch.tensor([
+    token_to_id["what"],
+    token_to_id["is"],
+    token_to_id["statquest"],
+    token_to_id["<EOS>"]
+])
+
+input_length = model_input.size(dim=0)
+
+predictions = model(model_input)
+predicted_id = torch.tensor([torch.argmax(predictions[-1, :])])
+predicted_ids = predicted_id
+
+max_length = 6
+for i in range(input_length, max_length):
+    if predicted_id == token_to_id["<EOS>"]:
+        break
+
+    model_input = torch.cat((model_input, predicted_id))
+
+    predictions = model(model_input)
+    predicted_id = torch.tensor([torch.argmax(predictions[-1, :])])
+    predicted_ids = torch.cat((predicted_ids, predicted_id))
+
+print("Predicted Tokens:\n")
+for id in predicted_ids:
+    print("\t", id_to_token[id.item()])
+
 
 
         
