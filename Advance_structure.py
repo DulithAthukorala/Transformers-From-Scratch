4f3ca78460168b3fa0d48d3ceaf9a2d6781bafa8
Dulith Athukorala <dulithathukorala678@gmail.com>
2026-01-26 08:09:18 +0530

    Refactor SelfAttention class to improve variable naming and consistency in embedding calculations

--- Advance_structure.py
+++ Advance_structure.py
@@ -5,16 +5,16 @@ import torch.nn as nn
 class SelfAttention(nn.Module):
     def __init__(self, embed_size, heads):
         super().__init__()
-        self.embed_size = embed_size  # word embedding size (one word = 1 dimensional vector) , (sentence = multiple words = 2D matrix) , (batch of sentences = 3D matrix)
+        self.embed_size = embed_size  # word embedding size (how many numbers represent a word)
         self.heads = heads # number of heads (parallel attention layers)
-        self.head_dim = embed_size // heads # dimension of each head
+        self.embedding_numbers_per_head = embed_size // heads # features per head
 
-        assert (self.head_dim * heads == embed_size), "Embedding size needs to be divisible by heads"
+        assert (self.embedding_numbers_per_head * heads == embed_size), "Embedding size needs to be divisible by heads"
 
-        self.queries = nn.Linear(self.head_dim, self.head_dim, bias=False) # nn.Linear -> initialize random weight matrix
-        self.keys = nn.Linear(self.head_dim, self.head_dim, bias=False)
-        self.values = nn.Linear(self.head_dim, self.head_dim, bias=False)
-        self.fc_out = nn.Linear(heads * self.head_dim, embed_size)  # final output linear layer
+        self.queries = nn.Linear(self.embedding_numbers_per_head, self.embedding_numbers_per_head, bias=False) # nn.Linear -> initialize random weight matrix
+        self.keys = nn.Linear(self.embedding_numbers_per_head, self.embedding_numbers_per_head, bias=False)
+        self.values = nn.Linear(self.embedding_numbers_per_head, self.embedding_numbers_per_head, bias=False)
+        self.fc_out = nn.Linear(heads * self.embedding_numbers_per_head, embed_size)  # final output linear layer
 
     def forward(self, values, keys, queries, mask):
 
