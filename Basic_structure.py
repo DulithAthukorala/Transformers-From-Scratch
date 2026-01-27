b395a378d550fc90187717df8e8ff9480c0bc354
Dulith Athukorala <dulithathukorala678@gmail.com>
2026-01-27 08:33:21 +0530

    Refactor PositionEncoding and Attention classes for consistency in parameter naming and improve clarity in the DecoderOnlyTransformer initialization

--- Basic_structure.py
+++ Basic_structure.py
@@ -44,15 +44,15 @@ dataset = TensorDataset(inputs, labels)
 dataloader = DataLoader(dataset)
 
 class PositionEncoding(nn.Module):
-    def __init__(self, d_model=2, max_len=6):  # Embedding size = 2 , token length = 6
+    def __init__(self, embed_size=2, max_length=6):  # Embedding size = 2 , token length = 6
         super().__init__() # initialize the parent class
 
-        pe = torch.zeros(max_len, d_model)  # postion encoding matrix of size (6, 2)
+        pe = torch.zeros(max_length, embed_size)  # postion encoding matrix of size (6, 2)
 
-        position = torch.arange(start=0, end=max_len).unsqueeze(1)  # position vector of size (6,1)
-        embedding_index = torch.arange(start=0, end=d_model, step=2)  # step 2 bcz, in the power of 2i anyways its multiples of 2
+        position = torch.arange(start=0, end=max_length).unsqueeze(1)  # position vector of size (6,1)
+        embedding_index = torch.arange(start=0, end=embed_size, step=2)  # step 2 bcz, in the power of 2i anyways its multiples of 2
 
-        div_term =  1/torch.tensor(10000.0)**(embedding_index/d_model)  # denominator term
+        div_term =  1/torch.tensor(10000.0)**(embedding_index/embed_size)  # denominator term
 
         pe[:, 0::2] = torch.sin(position * div_term)  # even index positon 0,2,4
         pe[:, 1::2] = torch.cos(position * div_term)  # odd index position 1,3,5
@@ -61,24 +61,24 @@ class PositionEncoding(nn.Module):
 
 
     def forward(self, word_embeddings):
-        return word_embeddings + self.pe[:word_embeddings.size(0), :] # Rows from 0 up to L-1 , all columns  # word_embedding.size(0) = max_len
+        return word_embeddings + self.pe[:word_embeddings.size(0), :] # Rows from 0 up to L-1 , all columns  # word_embedding.size(0) = max_length
         
 
 class Attention(nn.Module):
-    def __init__(self, d_model=2):
+    def __init__(self, embed_size=2):
         super().__init__()
         
-        self.w_q = nn.Linear(in_features=d_model, out_features=d_model, bias=False)  # weight matrix for query
-        self.w_k = nn.Linear(in_features=d_model, out_features=d_model, bias=False)  # weight matrix for key
-        self.w_v = nn.Linear(in_features=d_model, out_features=d_model, bias=False)  # weight matrix for value
+        self.w_q = nn.Linear(in_features=embed_size, out_features=embed_size, bias=False)  # weight matrix for query
+        self.w_k = nn.Linear(in_features=embed_size, out_features=embed_size, bias=False)  # weight matrix for key
+        self.w_v = nn.Linear(in_features=embed_size, out_features=embed_size, bias=False)  # weight matrix for value
 
         self.row_dim = 0 
         self.col_dim = 1
 
     def forward(self, encodings_for_q, encodings_for_k, encodings_for_v, mask=None):
-        q = self.w_q(encodings_for_q)  # (L, d_model)
-        k = self.w_k(encodings_for_k)  # (L, d_model)
-        v = self.w_v(encodings_for_v)  # (L, d_model)
+        q = self.w_q(encodings_for_q)  # (L, embed_size)
+        k = self.w_k(encodings_for_k)  # (L, embed_size)
+        v = self.w_v(encodings_for_v)  # (L, embed_size)
 
         sims = torch.matmul(q, k.transpose(dim0=self.row_dim, dim1=self.col_dim)) 
 
@@ -89,25 +89,25 @@ class Attention(nn.Module):
 
         attention_percents = F.softmax(scaled_sims, dim=self.col_dim)  # softmax along columns (but works row-wise)
 
-        attention_scores = torch.matmul(attention_percents, v) # (L, d_model)
+        attention_scores = torch.matmul(attention_percents, v) # (L, embed_size)
 
         return attention_scores
 
 
 class DecoderOnlyTransformer(L.LightningModule):
-    def __init__(self, num_tokens=4, d_model=2, max_len=6): #num_tokens=how many unique tokens in the vocabulary
+    def __init__(self, src_vocab_size=4, embed_size=2, max_length=6): #src_vocab_size=how many unique tokens in the vocabulary
         super().__init__()
 
-        self.we = nn.Embedding(num_embeddings=num_tokens, embedding_dim=d_model)  # create a lookup table for word embeddings (num_tokens, d_model)
-        self.pe = PositionEncoding(max_len=max_len,d_model=d_model)  # position info for each token/word (max_len, d_model)
-        self.self_attention = Attention(d_model=d_model)  # 
+        self.word_embedding = nn.Embedding(num_embeddings=src_vocab_size, embedding_dim=embed_size)  # create a lookup table for word embeddings (src_vocab_size, embed_size)
+        self.pe = PositionEncoding(max_length=max_length,embed_size=embed_size)  # position info for each token/word (max_length, embed_size)
+        self.self_attention = Attention(embed_size=embed_size)  # 
 
-        self.fc_layer = nn.Linear(in_features=d_model, out_features=num_tokens)  # final linear layer to project to vocab size
+        self.fc_layer = nn.Linear(in_features=embed_size, out_features=src_vocab_size)  # final linear layer to project to vocab size
 
         self.loss = nn.CrossEntropyLoss()  # cross entropy loss for classification
 
     def forward(self, token_ids): # token_id -> where the token in the vocabulary
-        word_embeddings = self.we(token_ids) 
+        word_embeddings = self.word_embedding(token_ids) 
         positional_encoded = self.pe(word_embeddings)
 
         mask = torch.tril(torch.ones((token_ids.size(dim=0), token_ids.size(dim=0))))
@@ -132,7 +132,7 @@ class DecoderOnlyTransformer(L.LightningModule):
         return loss
 
 
-model = DecoderOnlyTransformer(num_tokens=len(token_to_id), d_model=2, max_len=6)
+model = DecoderOnlyTransformer(src_vocab_size=len(token_to_id), embed_size=2, max_length=6)
 trainer = L.Trainer(max_epochs=30)
 trainer.fit(model, train_dataloaders=dataloader)
 
@@ -149,8 +149,8 @@ predictions = model(model_input)
 predicted_id = torch.tensor([torch.argmax(predictions[-1, :])])
 predicted_ids = predicted_id
 
-max_length = 6
-for i in range(input_length, max_length):
+max_lengthgth = 6
+for i in range(input_length, max_lengthgth):
     if predicted_id == token_to_id["<EOS>"]:
         break
 
