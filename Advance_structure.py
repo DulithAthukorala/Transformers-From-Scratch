9be3be51f23725805736baaf18697483e58f93f0
Dulith Athukorala <dulithathukorala678@gmail.com>
2026-01-27 16:31:28 +0530

    Implement Transformer class with encoder-decoder architecture and masking methods; enhance comments for clarity

--- Advance_structure.py
+++ Advance_structure.py
@@ -115,7 +115,7 @@ class Encoder(nn.Module):
         out = self.dropout(self.word_embedding(x) + self.position_embedding(positions))  # add word embeddings and position embeddings
 
         for layer in self.layers:
-            out = layer(out, out, out, mask) # In Encoder: value, key, query are all the same
+            out = layer(out, out, out, mask) # In Encoder: value, key, query are all the same #calls TransformerBlock.forward()s
 
         return out
 
@@ -141,7 +141,7 @@ class DecoderBlock(nn.Module):
 class Decoder(nn.Module):
     def __init__(
         self,
-        trg_vocab_size,
+        trg_vocab_size, # how many unique words in target vocabulary (ex: French vocab size)
         embed_size,
         num_layers,
         heads,
@@ -175,5 +175,88 @@ class Decoder(nn.Module):
         out = self.fc_out(x)
 
         return out
+    
+class Transformer(nn.Module):
+    def __init__(
+        self,
+        src_vocab_size,
+        trg_vocab_size,
+        src_pad_idx,
+        trg_pad_idx,
+        embed_size=512,
+        num_layers=6,
+        forward_expansion=4,
+        heads=8,
+        dropout=0,
+        device="cuda",
+        max_length=100,
+    ):
+        super().__init__()
+
+        self.encoder = Encoder(
+            src_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            device,
+            forward_expansion,
+            dropout,
+            max_length,
+        )
+
+        self.decoder = Decoder(
+            trg_vocab_size,
+            embed_size,
+            num_layers,
+            heads,
+            forward_expansion,
+            dropout,
+            device,
+            max_length,
+        )
+
+        self.src_pad_idx = src_pad_idx # padding index for english sentences to make them of equal length
+        self.trg_pad_idx = trg_pad_idx # padding index for french sentences to make them of equal length
+        self.device = device
+
+    def make_src_mask(self, src):
+        src_mask = (src != self.src_pad_idx).unsqueeze(1).unsqueeze(2)
+        # (N, 1, 1, src_len)
+        return src_mask.to(self.device)
+
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.shape
+        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
+            N, 1, trg_len, trg_len
+        )
+        # (N, 1, trg_len, trg_len)
+        return trg_mask.to(self.device)
+
+    def forward(self, src, trg):
+        src_mask = self.make_src_mask(src)
+        trg_mask = self.make_trg_mask(trg)
+        enc_src = self.encoder(src, src_mask)
+        out = self.decoder(trg, enc_src, src_mask, trg_mask)
+        return out
+    
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    x = torch.tensor([[1, 5, 6, 4, 3, 9, 5, 2, 0], [1, 8, 7, 3, 4, 5, 6, 7, 2]]).to(
+        device
+    )
+    trg = torch.tensor([[1, 7, 4, 3, 5, 9, 2, 0], [1, 5, 6, 2, 4, 7, 6, 2]]).to(device)
+
+    src_pad_idx = 0
+    trg_pad_idx = 0
+    src_vocab_size = 10
+    trg_vocab_size = 10
+    model = Transformer(src_vocab_size, trg_vocab_size, src_pad_idx, trg_pad_idx, device=device).to(
+        device
+    )
+    out = model(x, trg[:, :-1])
+    print(out.shape)
     
 
